4e1f69a101e416e5f06cd612d5e04cc05ebee85b
abhishekamralkar <abhishekamralkar@gmail.com>
2020-08-23 12:20:18 +0530

    adding the emacs themes

--- emacs/init-local.el
+++ emacs/init-local.el
@@ -1,11 +1,33 @@
+;;; init-local.el --- My emacs config -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
 (set-face-attribute 'default nil
-                    :family "Hack Nerd Font Regular"
-                    :height 120
+                    :family "Fira Code Regular"
+                    :height 112
                     :weight 'normal
                     :width 'normal)
 
 
 
-(add-to-list 'load-path "/home/aaa/.emacs.d/themes/color-theme-sanityinc-tomorrow")
-(require 'color-theme-sanityinc-solarized)
+(add-to-list 'load-path "/home/aaa/.emacs.d/themes")
+(require 'ujelly-theme)
+(desktop-save-mode 0)
+
+
+
+(setq org-agenda-files (list "~/.emacs.d/org/todo.org"
+                             "~/.emacs.d/org/learning.org" 
+                             "~/.emacs.d/org/home.org"))
+
+
+(add-hook 'rust-mode-hook 'cargo-minor-mode)
+
+(setq racer-cmd "~/.cargo/bin/racer") ;; Rustup binaries PATH
+(setq racer-rust-src-path "~/Code/Rust/rust/src") ;; Rust source code PATH
+
+(add-hook 'rust-mode-hook #'racer-mode)
+(add-hook 'racer-mode-hook #'eldoc-mode)
+(add-hook 'racer-mode-hook #'company-mode)
 (provide 'init-local)
+;;; init-local.el ends here
