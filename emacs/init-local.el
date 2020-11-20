d0f9d975832603f5038247202ad51c8a64c1836a
Abhishek Amralkar <abhishekamralkar@gmail.com>
2020-11-20 22:02:48 +0530

    update the gnome keybinding

--- emacs/init-local.el
+++ emacs/init-local.el
@@ -5,8 +5,16 @@
                     :width 'normal)
 
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-(load-theme 'gruvbox-dark-hard t)
+(load-theme 'dracula t)
 
+;; add package path 
+(add-to-list 'load-path "~/.emacs.d/site-lisp/use-package")
+(require 'use-package)
+
+(with-eval-after-load 'info
+  (info-initialize)
+  (add-to-list 'Info-directory-list
+               "~/.emacs.d/site-lisp/use-package/"))
 (require 'use-package)
 
 (use-package fira-code-mode
@@ -17,62 +25,6 @@
 
 
 
-;; golang
-(setq lsp-gopls-staticcheck t)
-(setq lsp-eldoc-render-all t)
-(setq lsp-gopls-complete-unimported t)
-
-(use-package lsp-mode
-  :ensure t
-  :commands (lsp lsp-deferred)
-  :hook (go-mode . lsp-deferred))
-
-;;Set up before-save hooks to format buffer and add/delete imports.
-;;Make sure you don't have other gofmt/goimports hooks enabled.
-
-(defun lsp-go-install-save-hooks ()
-  (add-hook 'before-save-hook #'lsp-format-buffer t t)
-  (add-hook 'before-save-hook #'lsp-organize-imports t t))
-(add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
-
-;;Optional - provides fancier overlays.
-
-(use-package lsp-ui
-  :ensure t
-  :commands lsp-ui-mode
-  :init
-)
-
-;;Company mode is a standard completion package that works well with lsp-mode.
-;;company-lsp integrates company mode completion with lsp-mode.
-;;completion-at-point also works out of the box but doesn't support snippets.
-
-(use-package company
-  :ensure t
-  :config
-  (setq company-idle-delay 0)
-  (setq company-minimum-prefix-length 1))
-
-(use-package company-lsp
-  :ensure t
-  :commands company-lsp)
-
-;;Optional - provides snippet support.
-
-(use-package yasnippet
-  :ensure t
-  :commands yas-minor-mode
-  :hook (go-mode . yas-minor-mode))
-
-;;lsp-ui-doc-enable is false because I don't like the popover that shows up on the right
-;;I'll change it if I want it back
-
-
-(setq lsp-ui-doc-enable nil
-      lsp-ui-peek-enable t
-      lsp-ui-sideline-enable t
-      lsp-ui-imenu-enable t
-      lsp-ui-flycheck-enable t)
 
 ;;no decoration
 (set-frame-parameter nil 'undecorated t)
