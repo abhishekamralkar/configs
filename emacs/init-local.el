6333fe0a3572f19e3275dc973987ae8ac8867bcd
Abhishek Amralkar <abhishekamralkar@gmail.com>
2022-06-01 19:17:17 +0530

    update the python keybinding

--- emacs/init-local.el
+++ emacs/init-local.el
@@ -1,13 +1,12 @@
-(set-face-attribute 'default nil
-                    :family "Fira Code"
-                    :height 90
-                    :weight 'normal
-                    :width 'normal)
+;;; init-local.el --- Configure default locale -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
 
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-(load-theme 'dracula t)
+(load-theme 'gruvbox t)
 
-;; add package path 
+;; add package path
 (add-to-list 'load-path "~/.emacs.d/site-lisp/use-package")
 (require 'use-package)
 
@@ -17,30 +16,26 @@
                "~/.emacs.d/site-lisp/use-package/"))
 (require 'use-package)
 
-(use-package fira-code-mode
-  :custom (fira-code-mode-disabled-ligatures '("[]" "x"))  ; ligatures you don't want
-  :hook prog-mode)
+(desktop-save-mode 0)
 
+
+(set-face-attribute 'default nil :font "Hack-10" )
 
 ;; elpy
 (use-package elpy
-  :ensure t
-  :init
-  (elpy-enable))
+             :ensure t
+             :init
+             (elpy-enable))
 
 ;; set default python3
 (setq python-shell-interpreter "python3"
       python-shell-interpreter-args "-i")
 
-(setq python-indent-guess-indent-offset t)  
+(setq python-indent-guess-indent-offset t)
 (setq python-indent-guess-indent-offset-verbose nil)
-(desktop-save-mode 0)
-
-
-
 
 ;;no decoration
-(set-frame-parameter nil 'undecorated t)
+;;(set-frame-parameter nil 'undecorated t)
 
 (provide 'init-local)
 ;;; init-local.el ends here
