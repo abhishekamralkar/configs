6261dfde3a647ea2f704379f32734322cd8e7c97
Abhishek Amralkar <abhishekamralkar@gmail.com>
2022-07-02 00:23:06 +0530

    emacs: add golsp

---
+++ emacs/init-go.el
@@ -0,0 +1,21 @@
+;;; init-go.el --- Configure default locale -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+;; Company mode
+(setq company-idle-delay 0)
+(setq company-minimum-prefix-length 1)
+
+;; Go - lsp-mode
+;; Set up before-save hooks to format buffer and add/delete imports.
+(defun lsp-go-install-save-hooks ()
+  (add-hook 'before-save-hook #'lsp-format-buffer t t)
+  (add-hook 'before-save-hook #'lsp-organize-imports t t))
+(add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
+
+;; Start LSP Mode and YASnippet mode
+(add-hook 'go-mode-hook #'lsp-deferred)
+(add-hook 'go-mode-hook #'yas-minor-mode)
+
+(provide 'init-go)
+;;; init-go.el ends here
